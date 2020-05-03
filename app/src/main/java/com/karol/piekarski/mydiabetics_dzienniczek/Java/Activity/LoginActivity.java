e8a6ab9007400dfe7daf92df70dd02cb0c84d358
piekarz432karl <piekarz432karl@wp.pl>
2020-05-03 17:25:59 +0200

    Logowanie przez googla.

--- app/src/main/java/com/karol/piekarski/mydiabetics_dzienniczek/Java/Activity/LoginActivity.java
+++ app/src/main/java/com/karol/piekarski/mydiabetics_dzienniczek/Java/Activity/LoginActivity.java
@@ -4,23 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 import com.karol.piekarski.mydiabetics_dzienniczek.Java.Class.Repository;
 import com.karol.piekarski.mydiabetics_dzienniczek.Java.Interfaces.Validate;
 import com.karol.piekarski.mydiabetics_dzienniczek.R;
 
-public class LoginActivity extends AppCompatActivity implements Validate {
+public class LoginActivity extends AppCompatActivity implements Validate{
+
+
+    static final int RC_SIGN_IN = 0;
 
     private EditText login;
     private EditText password;
     private TextView signUp;
     private Button btnLogin;
     private TextView forgotPassword;
+    private ImageView google;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +45,13 @@ public class LoginActivity extends AppCompatActivity implements Validate {
         signUp=findViewById(R.id.signUp);
         btnLogin=findViewById(R.id.changePassword);
         forgotPassword=findViewById(R.id.forgotPassword);
+        google=findViewById(R.id.googleLogin);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +61,6 @@ public class LoginActivity extends AppCompatActivity implements Validate {
 
             }
         });
-
-
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,12 +77,46 @@ public class LoginActivity extends AppCompatActivity implements Validate {
             }
         });
 
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
     }
 
 
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 
     @Override
@@ -108,5 +160,7 @@ public class LoginActivity extends AppCompatActivity implements Validate {
 
         }
     }
+    
+
 
 }
